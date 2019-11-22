01c62ed8794513725a97a2262b0423ddef2ab73d
michael <michael.catchen@colorado.edu>
2019-11-22 11:10:49 -0700

    ok

--- mutation_test.jl
+++ mutation_test.jl
@@ -2,7 +2,7 @@ include("./src/include.jl")
 
 param_dict = Dict(
     "ipc"       => [10],
-    "m"         => collect(10^-5),
+    "m"         => collect(exp(-9):exp(-7):exp(-4)),
     "n_pops"    => [20],
     "k"         => [2000],
     "ibd_str"   => [8.0],
@@ -13,7 +13,7 @@ treatment_df = create_treatments(param_dict)
 
 batch_fits_multicore(
     treatment_df,
-    1,
+    20,
     num_generations=20000,
     replicates_per_treatment = 100,
     log_frequency = 500,
