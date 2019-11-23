8bbdb1e6c74c0343a066e82fe8e842195fc40452
michael <michael.catchen@colorado.edu>
2019-11-23 12:17:08 -0700

    ok

---
+++ gauss.jl
@@ -0,0 +1,21 @@
+include("./src/include.jl")
+
+param_dict = Dict(
+    "ipc"       => [10],
+    "m"         => collect(range(exp(-9), exp(-4), length=100)),
+    "n_pops"    => [20],
+    "k"         => [2000],
+    "ibd_str"   => [3.0, 8.0, 15.0],
+    "mutation_rate" => [10^-7]
+)
+
+treatment_df = create_treatments(param_dict)
+
+batch_fits(
+    treatment_df,
+    num_generations=10000,
+    replicates_per_treatment = 50,
+    log_frequency = 500,
+    base_random_seed = 5,
+    dispersal_kernel_type=@gauss_diskern
+)
